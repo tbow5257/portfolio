5780b91526bfa7c3231374ffe1ac1cabf756b298
tbow5257 <133tjb@gmail.com>
2018-03-28 23:27:46 -0700

    Frontpage sections setup

--- src/components/Gradient/index.js
+++ src/components/Gradient/index.js
@@ -8,27 +8,31 @@ height: 100vh;
 z-index: -999;
 position: absolute;
   
-background: linear-gradient(48deg, #2ba84a, #040f0f, #248232, #2d3a3a);
-background-size: 800% 800%;
+  
+  
+background: linear-gradient(33deg, #45ffd0, #227a63);
+background-size: 400% 400%;
 
--webkit-animation: movingthing 30s ease infinite;
--moz-animation: movingthing 30s ease infinite;
-animation: movingthing 30s ease infinite;
+-webkit-animation: AnimationName 23s ease infinite;
+-moz-animation: AnimationName 23s ease infinite;
+animation: AnimationName 23s ease infinite;
 
-@-webkit-keyframes movingthing {
-    0%{background-position:4% 0%}
-    50%{background-position:97% 100%}
-    100%{background-position:4% 0%}
+@-webkit-keyframes AnimationName {
+    0%{background-position:0% 77%}
+    50%{background-position:100% 24%}
+    100%{background-position:0% 77%}
 }
-@-moz-keyframes movingthing {
-    0%{background-position:4% 0%}
-    50%{background-position:97% 100%}
-    100%{background-position:4% 0%}
+@-moz-keyframes AnimationName {
+    0%{background-position:0% 77%}
+    50%{background-position:100% 24%}
+    100%{background-position:0% 77%}
 }
-@keyframes movingthing { 
-    0%{background-position:4% 0%}
-    50%{background-position:97% 100%}
-    100%{background-position:4% 0%}
+@keyframes AnimationName { 
+    0%{background-position:0% 77%}
+    50%{background-position:100% 24%}
+    100%{background-position:0% 77%}
+}
+    
 }`
 
 const Gradient = () => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,32 +4,85 @@ import styled from 'styled-components'
 
 import Gradient from '../components/Gradient'
 
-const Section = styled.div`
+import Icon from '../assets/noun.png'
+import Email from '../assets/social/email.png'
+import Github from '../assets/social/github.png'
+import Twitter from '../assets/social/twitter.png'
+import Placeholder from '../assets/placeholder.jpg'
 
+
+
+const Container = styled.div`
+display: flex;
+flex-flow: column;
+text-align: center;
+justify-content: space-between;
+align-content: space-between;
+min-width: 100%;
+min-height: 100%;
+height: 100vh;
+
+`;
+
+const Header = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-flow: column;
+padding-top: 1em;
+    
+    img {
+      max-width: 35px;
+      margin-bottom: .45em;
+    }
+    
+    p {
+    color: white;
+    mix-blend-mode: difference;
+    text-align: center;
+    }
+`;
+
+const ProjectsContainer = styled.div`
 
 h1 {
-color: white;
-mix-blend-mode: difference;
+font-size: 3.5em
 }
 
-p {
-color: white;
-mix-blend-mode: difference;
+img {
+height: 50vh;
 }
+`;
 
+const Footer = styled.div`
+    img {
+    height: 35px;
+    }
 `;
 
 const IndexPage = () => (
     <div>
-        <Gradient />
-            <Section>
+        <Gradient/>
+        <Container>
+            <Header>
                 <div>
-                    <h1>Thomas Bowen Dev</h1>
-                    <p>Welcome to your new Gatsby site.</p>
-                    <p>Now go build something great.</p>
-                    <Link to="/page-2/">Go to page 2</Link>
+                    <img src={Icon}/>
                 </div>
-            </Section>
+                <div>
+                    <p>Thomas Bowen Dev</p>
+                </div>
+            </Header>
+            <ProjectsContainer>
+                <h1>jQuery, PHP</h1>
+                <img src={Placeholder} />
+            </ProjectsContainer>
+            <Footer>
+                <img src={Email}/>
+                <img src={Github}/>
+                <img src={Twitter}/>
+
+            </Footer>
+        </Container>
     </div>
 )
 
